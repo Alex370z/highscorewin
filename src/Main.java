3fa8027694c72ae425a74bfa5253174ed22b7c49
Alex370z <jetarmorz@gmail.com>
2025-04-17 10:28:39 -0700

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // asks for keyboard input
+        System.out.println("ENTER TEAM AND SCORE. (EX HOME:AWAY|_:_) ");
+        String in = input.nextLine();
+
+
+        // split input into 2
+        String[] Split = in.split("\\|");
+
+
+        // organize display teams and scores
+        String[] teams = Split[0].split(":");
+        String[] scores = Split[1].split(":");
+
+
+        // split teams
+        String home = teams[0];
+        String away = teams[1];
+
+
+        // split scores
+        int score1 = Integer.parseInt(scores[0]);
+        int score2 = Integer.parseInt(scores[1]);
+
+
+        // if score 1 is greater than 2
+        if (score1 > score2){
+            System.out.printf("WINNER: %s", home);
+        // if score 2 is greater than score 1
+        }else if (score2 > score1){
+            System.out.printf("WINNER: %s", away);
+
+        }else{
+            System.out.println("ITS A DRAW");
         }
+
     }
 }
